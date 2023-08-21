f61a1a93c656a62094d5398a63429ed04cca8446
PLK <unrecht.official@gmail.com>
2023-08-21 22:53:29 +0200

    🎉 Initial commit.

---
+++ src/main/java/de/plk/plugin/SkywarsPlugin.java
@@ -0,0 +1,24 @@
+package de.plk.plugin;
+
+import de.plk.core.api.AbstractSpicySpigot;
+
+/**
+ * @author SoftwareBuilds
+ * @since 21.08.2023 22:52
+ * Copyright © 2023 | SoftwareBuilds | All rights reserved.
+ */
+public final class SkywarsPlugin extends AbstractSpicySpigot {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+    }
+
+}
